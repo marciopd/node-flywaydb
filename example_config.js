9c22e35939158a910f48c2a77129c71b3cdf6313
Mark Gardner <mark@outer-limit.com>
2018-09-28 17:01:15 -0600

    WIP refactor

--- example_config.js
+++ example_config.js
@@ -6,5 +6,18 @@ module.exports = {
 	locations: 'filesystem:sql/migrations',
 	user: env.PGUSER,
 	password: env.PGPASSWORD,
-	sqlMigrationSuffix: '.pgsql'
+    sqlMigrationSuffix: '.pgsql',
+    version: '4.0.3', // optional, empty or missing will download the latest
+    JAVA_OPTS: [ // optional, use this if you are needing to configure a mavinPlugin
+        '-Djava.util.logging.config.file=./conf/logging.properties',
+    ],
+    mavinPlugins: [{ // optional, use to add any plugins (ie. logging)
+        groupId: 'org.slf4j',
+        artifactId: 'slf4j-api',
+        version: '1.7.25'
+    }, {
+        groupId: 'org.slf4j',
+        artifactId: 'slf4j-jdk14',
+        version: '1.7.25'
+    }]
 };
