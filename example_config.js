685f31c0f4fdf96e904370b79b3b07b5940455a0
Mark Gardner <mark@outer-limit.com>
2019-04-08 21:13:52 -0600

    Finishing changes

--- example_config.js
+++ example_config.js
@@ -1,20 +1,25 @@
 var env = process.env;
 
 module.exports = {
-    url: `jdbc:postgresql://${env.PGHOST}:${env.PGPORT}/${env.PGDATABASE}`,
-    schemas: 'public',
-    locations: 'filesystem:sql/migrations',
-    user: env.PGUSER,
-    password: env.PGPASSWORD,
-    sqlMigrationSuffix: '.pgsql',
+    flywayArgs: {
+        url: `jdbc:postgresql://${env.PGHOST}:${env.PGPORT}/${env.PGDATABASE}`,
+        schemas: 'public',
+        locations: 'filesystem:sql/migrations',
+        user: env.PGUSER,
+        password: env.PGPASSWORD,
+        sqlMigrationSuffix: '.pgsql',
+    },
+    // Use to configure environment variables used by flyway
+    env: {
+        JAVA_ARGS: '-Djava.util.logging.config.file=./conf/logging.properties',
+    },
     version: '4.0.3', // optional, empty or missing will download the latest
-    JAVA_OPTS: [ // optional, use this if you are needing to configure a mavinPlugin
-        '-Djava.util.logging.config.file=./conf/logging.properties',
-    ],
     mavinPlugins: [{ // optional, use to add any plugins (ie. logging)
         groupId: 'org.slf4j',
         artifactId: 'slf4j-api',
-        version: '1.7.25'
+        version: '1.7.25',
+        // This can be a specifc url to download that may be different then the auto generated url.
+        downloadUrl: 'https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.25/slf4j-api-1.7.25.jar',
     }, {
         groupId: 'org.slf4j',
         artifactId: 'slf4j-jdk14',
